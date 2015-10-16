0677036df4b5a305681bc9d3c0afcc1a5537ca11
Viktorija <gulbickaja.viktorija@gmail.com>
2015-10-16 18:38:41 +0300

    popup

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,66 @@
 console.log('example js file');
 $(document).ready(function(){
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
-			$('.back-to-top').fadeIn();
-		} else {
-			$('.back-to-top').fadeOut();
-		}
+		$(".open-popup-link").magnificPopup({
+			type:'inline'
+	 	});
+
+		$('.gallery').each(function() { // the containers for all your galleries
+		    $(this).magnificPopup({
+		        delegate: 'a', // the selector for gallery item
+		        type: 'image',
+		        gallery: {
+		          enabled:true
+		        }
+		    });
+		});
+
+// button scroll to top
+		$(window).scroll(function(){
+			if ($(this).scrollTop() > 100) {
+				$('.back-to-top').fadeIn();
+			} else {
+				$('.back-to-top').fadeOut();
+			}
+		});
+
+		$('.back-to-top').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
 	});
-	
-	$('.back-to-top').click(function(){
-	$('html, body').animate({scrollTop : 0},800);
-	return false;
+
+
+
+// gallery
+$('.theme-popup').magnificPopup({
+    items: [
+      {
+        src: 'images/pict1.png',
+        title: '<a href="" class="theme-link">View the theme</a>'
+      },
+			{
+				src: 'images/pict2.png',
+				title: '<a href="" class="theme-link">View the theme</a>'
+			}
+		],
+		gallery: {
+		enabled: true
+		},
+		type: 'image'
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
